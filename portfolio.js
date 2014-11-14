207c220796f1a834f89524409e473780ff067e03
bttf <adnanchowdhury88@gmail.com>
2014-11-14 19:28:16 +0000

    iterative logic works

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "entries": [{
+    "title": "chewbonga blog",
+    "date": "2014-11-08",
+    "points": ["Development blog created for purposes of keeping record, sharing findings and inviting criticism",
+      "Built with Ember.js + Express + Node + MongoDB",
+      "Fully-responsive for mobile browsers by utilizing Bootstrap",
+      "Code available on <a href=\"#\">GitHub</a>"],
+    "img": "/img/chewbonga.jpg"
+  }, {
+    "title": "test",
+    "date": "2014-08-08",
+    "points": ["test",
+      "another test"],
+    "img": "/img/chewbonga.jpg"
+  }, {
+    "title": "test",
+    "date": "2014-08-08",
+    "points": ["test",
+      "another test"],
+    "img": "/img/chewbonga.jpg"
+  }]
+};
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var mockData = [
-  {
-    title: 'Sitting on my Ass',
-    datesActive: 'Sept. 15 1988 - Present'
-  },
-  {
-    title: 'Bitching and Whining',
-    datesActive: 'Constantly'
-  }
-];
-
-
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', data: mockData });
+  res.render('index', { entries: require('../portfolio').entries });
 });
 
 module.exports = router;
